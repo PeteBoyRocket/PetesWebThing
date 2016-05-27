9cfe042f9ce1a87d9a9583cc8ab4bf0a6a5ded17
PeteBoyRocket <peteboyrocket@hotmail.com>
2016-05-27 23:40:37 +0100

    added add update methods

--- DBRepo/CardRepository.cs
+++ DBRepo/CardRepository.cs
@@ -21,5 +21,37 @@ namespace DBRepo
 				return cardsContext.Whites.Select(o => o.Sentence).ToList();
 			}
 		}
+
+		public void AddOrUpdateBlackCard(string originalSentence, string newSentence)
+		{
+			using (var cardsContext = new CardsContext())
+			{
+				var existingCard = cardsContext.Blacks.SingleOrDefault(o => o.Sentence == originalSentence);
+				if (existingCard == null)
+				{
+					cardsContext.Blacks.Add(new Black { Sentence = newSentence });
+				}
+				else
+				{
+					existingCard.Sentence = newSentence;
+				}
+			}
+		}
+
+		public void AddOrUpdateWhiteCard(string originalSentence, string newSentence)
+		{
+			using (var cardsContext = new CardsContext())
+			{
+				var existingCard = cardsContext.Whites.SingleOrDefault(o => o.Sentence == originalSentence);
+				if (existingCard == null)
+				{
+					cardsContext.Whites.Add(new White { Sentence = newSentence });
+				}
+				else
+				{
+					existingCard.Sentence = newSentence;
+				}
+			}
+		}
 	}
 }
