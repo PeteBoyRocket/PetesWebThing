ac97abfd6cc0683a34b3371c2f5febd3fb53551f
PeteBoyRocket <peteboyrocket@hotmail.com>
2016-10-19 19:22:51 +0100

    random tests

--- BusinessLogic/CardHandler.cs
+++ BusinessLogic/CardHandler.cs
@@ -1,44 +1,52 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace BusinessLogic
 {
-	public class CardHandler
-	{
-		private readonly ICardRepository _cardRepository;
+    public class CardHandler
+    {
+        private readonly ICardRepository _cardRepository;
+        // private readonly Random _random = new Random();
 
-		public CardHandler(ICardRepository cardRepository)
-		{
-			_cardRepository = cardRepository;
-		}
+        public CardHandler(ICardRepository cardRepository)
+        {
+            _cardRepository = cardRepository;
+        }
 
-		public string GetBlackCard()
-		{
-			return _cardRepository.GetBlackCards().FirstOrDefault();
-		}
+        public string GetBlackCard()
+        {
+            var cards = _cardRepository.GetBlackCards();
+            var index = new Random().Next(cards.Count());
 
-		public string GetWhiteCard()
-		{
-			return _cardRepository.GetWhiteCards().FirstOrDefault();
-		}
+            return cards.ElementAt(index);
+        }
 
-		public void AddBlackCard(string sentence)
-		{
-			_cardRepository.AddOrUpdateBlackCard(null, sentence);
-		}
+        public string GetWhiteCard()
+        {
+            var cards = _cardRepository.GetWhiteCards();
+            var index = new Random().Next(cards.Count());
 
-		public void AddWhiteCard(string sentence)
-		{
-			_cardRepository.AddOrUpdateWhiteCard(null, sentence);
-		}
+            return cards.ElementAt(index);
+        }
 
-		public void UpdateBlackCard(string originalSentence, string newSentence)
-		{
-			_cardRepository.AddOrUpdateBlackCard(originalSentence, newSentence);
-		}
+        public void AddBlackCard(string sentence)
+        {
+            _cardRepository.AddOrUpdateBlackCard(null, sentence);
+        }
 
-		public void UpdateWhiteCard(string originalSentence, string newSentence)
-		{
-			_cardRepository.AddOrUpdateWhiteCard(originalSentence, newSentence);
-		}
-	}
+        public void AddWhiteCard(string sentence)
+        {
+            _cardRepository.AddOrUpdateWhiteCard(null, sentence);
+        }
+
+        public void UpdateBlackCard(string originalSentence, string newSentence)
+        {
+            _cardRepository.AddOrUpdateBlackCard(originalSentence, newSentence);
+        }
+
+        public void UpdateWhiteCard(string originalSentence, string newSentence)
+        {
+            _cardRepository.AddOrUpdateWhiteCard(originalSentence, newSentence);
+        }
+    }
 }
